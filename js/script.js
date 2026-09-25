68e502a93beda336068db6f45fc4a38e5f80601f
Arafat070 <130224662+Arafat070@users.noreply.github.com>
2026-09-25 21:56:16 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -582,6 +582,7 @@ function showToast(message) {
 // ========== ФОРМА TELEGRAM ==========
 const BOT_TOKEN = '8582743328:AAFfKXDSBiyiVO7dAbCd1-e2ogcjkvWLgS4';
 const CHAT_ID = '992638774';
+const CHAT_ID = '6209207346';
 
 document.getElementById('responseForm').addEventListener('submit', async (e) => {
     e.preventDefault();
